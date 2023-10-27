00ca9e1d1a792dbb6dba8edb16529b325aa47276
Mikio Nakamaru <mikionakamaru@icloud.com>
2023-10-26 22:41:05 -0300

    Add files via upload

---
+++ Check Antivirus Status.ps1
@@ -0,0 +1,13 @@
+﻿$OutputMessage = @()
+
+$Antivirus = Get-CimInstance -Namespace root/SecurityCenter2 -ClassName AntivirusProduct
+
+foreach ($item in $Antivirus) {
+    $ComputerName = $env:COMPUTERNAME
+    $ProductName = $item.displayName
+
+    $Message = "ComputerName=$ComputerName;Antivirus=$ProductName"
+    $OutputMessage += $Message
+}
+
+$OutputMessage
